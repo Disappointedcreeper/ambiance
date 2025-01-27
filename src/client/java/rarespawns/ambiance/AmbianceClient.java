3683131d8355e34d8045dca243db9ef0a9d943b8
disappointedcreeper <disappintedcreeper@gmail.com>
2025-01-26 16:56:57 -0800

    Initial Commit

---
+++ src/client/java/rarespawns/ambiance/AmbianceClient.java
@@ -0,0 +1,10 @@
+package rarespawns.ambiance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmbianceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/rarespawns/ambiance/AmbianceDataGenerator.java
@@ -0,0 +1,11 @@
+package rarespawns.ambiance;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AmbianceDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
